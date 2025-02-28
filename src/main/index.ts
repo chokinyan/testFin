287118f5f65d91d2be2014e3aa44b3a40643fa2b
SaberL <55279869+chokinyan@users.noreply.github.com>
2025-02-28 10:58:44 +0100

    Enhance HTML structure and styling; add button and CSS for improved UI

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,14 +11,12 @@ function createWindow() {
       preload: path.join(__dirname, '../preload/preload.js'), // Chemin vers le preload
       contextIsolation: true, // Sécurité : isolement du contexte
       nodeIntegration: false, // Sécurité : désactive l'intégration de Node.js dans le renderer
+      devTools: true, // Ouvre les outils de développement
     },
   });
 
   // Charge le fichier HTML
   mainWindow.loadFile(path.join(__dirname, '../../public/index.html'));
-
-  // Ouvrir les outils de développement (optionnel)
-  mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
